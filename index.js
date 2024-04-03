5e40834037cf8bd10e5e649aba5eab75c8f0f70d
Moses <emperorskid@gmail.com>
2024-04-03 13:09:22 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    const searchLowerCase = string.toLowerCase(); 
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().includes(searchLowerCase);
+    });
+}
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(function(driver) {
+        const lowerDriver = driver.toLowerCase();
+        const lowerQuery = query.toLowerCase();
+        return lowerDriver.startsWith(lowerQuery);
+    });
+}
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.name === name;
+    });
+}
